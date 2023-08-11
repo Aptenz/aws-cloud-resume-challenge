f4ab8e8dbdc99cafbe0b95fc51c77a3e22c26d02
Nicholas Mason <aptenz@gmail.com>
2023-08-11 15:01:13 -0600

    Added infrastructure
    Also added links to resume

---
+++ infra/lambda/func.py
@@ -0,0 +1,21 @@
+import json
+import boto3
+
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table('cloudresume-test')
+def lambda_handler(event, context):
+    # get the item at key value 0
+    response = table.get_item(Key={
+        'id':'0'
+    })
+    
+    # get the views from the table and add one
+    views = response['Item']['views']
+    views += 1
+    print(views)
+    response = table.put_item(Item={
+        'id':'0',
+        'views':views
+    })
+    
+    return views
