f2176605559059490799fc4a37f6dc87a7fab45e
Nicholas Mason <aptenz@gmail.com>
2023-08-23 19:14:34 -0600

    Fixed backend cicd

--- infra/provider.tf
+++ infra/provider.tf
@@ -1,13 +1,14 @@
 terraform {
-    required_providers {
-        aws = {
-            version=">=4.9.0"
-            source ="hashicorp/aws"
-        }
+  required_providers {
+    aws = {
+      version = ">=4.9.0"
+      source  = "hashicorp/aws"
     }
+  }
 }
 
 provider "aws" {
-  region  = "us-east-1"
-  profile = "default"
+  region     = "us-east-1"
+  access_key = var.AWS_ACCESS_KEY_ID
+  secret_key = var.AWS_SECRET_ACCESS_KEY
 }
