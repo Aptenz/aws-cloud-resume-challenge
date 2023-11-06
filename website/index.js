1225165e22979f6a085eb287498cc02dd1369611
Nicholas Mason <aptenz@gmail.com>
2023-11-06 15:18:05 -0700

    Change function URL

--- website/index.js
+++ website/index.js
@@ -2,7 +2,7 @@
 
 const counter = document.querySelector(".counter-number");
 async function updateCounter() {
-    let response = await fetch("https://hermnnqy3apfsjhfrfdlbw23mi0kdfcl.lambda-url.us-east-1.on.aws/");
+    let response = await fetch("https://tz4rgbrnqgt2cxfvjlzfoy36me0gwhnx.lambda-url.us-east-1.on.aws/");
     let data = await response.json();
     counter.innerHTML = ` Visitors: ${data}`;
 }
