6c4d1e51894f7937bd5ca3b2ffe25406ebcb3f9c
Nicholas Mason <aptenz@gmail.com>
2024-01-17 19:59:16 -0700

    Updated cicd frontend and resume html

--- website/index.js
+++ website/index.js
@@ -4,7 +4,7 @@ const counter = document.querySelector(".counter-number");
 async function updateCounter() {
     let response = await fetch("https://tz4rgbrnqgt2cxfvjlzfoy36me0gwhnx.lambda-url.us-east-1.on.aws/");
     let data = await response.json();
-    counter.innerHTML = ` Visitors: ${data}`;
+    counter.innerHTML = `Visitors: ${data}`;
 }
 
 updateCounter();
